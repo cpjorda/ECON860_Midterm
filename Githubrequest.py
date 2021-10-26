cad3e506a167e9af92c821c26310e41013cf2b28
Caleb Jordan <calebjordan@Calebs-MacBook-Pro.local>
2021-10-25 23:36:14 -0400

    initial commit

--- Githubrequest.py
+++ Githubrequest.py
@@ -1,14 +1,34 @@
-import urllib.request
+import json
+import requests
+
 import os
-
-
 
 f = open("api_key", "r")
 api_key = f.read()
 f.close()
 
+f = open("username", "r")
+username = f.read()
+f.close()
 
 if not os.path.exists("githubjsondata"):
 	os.mkdir("githubjsondata")
 
-urllib.request.urlopen()
+githubsession = requests.Session()
+githubsession.auth = (username, api_key)
+
+
+access_point = "https://api.github.com"
+
+rate_limit_url = access_point + "/rate_limit"
+result = json.loads(githubsession.get(rate_limit_url).text)
+
+print(result)
+f.open("noduplicatedataset.csv", "r")
+users = f.read()
+f.close()
+for user in users:
+	user_url = access_point + "/users/" + user
+	result = json.loads(githubsession.get(user_url).text)
+	print(user)
+	print(result['public_repos'])
